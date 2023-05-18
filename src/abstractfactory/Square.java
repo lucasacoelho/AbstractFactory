015148f2097cd71af6a9c43362c035c34054794f
Lucas Almeida Coelho <89714517+lucasacoelho@users.noreply.github.com>
2023-05-18 19:47:12 -0300

    Add files via upload

---
+++ src/abstractfactory/Square.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abstractfactory;
+
+/**
+ *
+ * @author lucas
+ */
+public class Square {
+    
+}
